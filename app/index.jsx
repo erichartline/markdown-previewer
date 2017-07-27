6852bbe3a8719d58c2dd6b12015ac8b8d2c242ec
wildlifehexagon <crossthebreeze@gmail.com>
2017-07-27 09:35:00 -0500

    add markup, fix formatting, now fully working

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,40 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+let React = require('react');
+let ReactDOM = require('react-dom');
+let marked = require('marked');
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import './styles/app.scss';
 
 class Markdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: 'This is placeholder text.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({markdown: event.target.value});
+  }
+
+  rawMarkup() {
+    let rawMarkup = marked(this.state.markdown, {sanitize: true});
+    return {__html: rawMarkup};
+  }
+
   render() {
     return (
       <MuiThemeProvider>
         <Grid fluid>
         <Row>
-          <Col xs={6} md={3} lg={2}>
-            Hello, world!
+          <Col>
+            <textarea cols="50" rows="25" value={this.state.markdown} onChange={this.handleChange}></textarea>
           </Col>
-          <Col xs={6} md={3} lg={2}>
-            Hello, world!
+          <Col>
+            <div dangerouslySetInnerHTML={this.rawMarkup()}></div>
           </Col>
         </Row>
       </Grid>
